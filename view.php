6d62341140c8505090717411519be24c752881f8
Sean <mail@seanslater.com>
2015-11-02 04:26:46 +1030

    Cats and Dogs view pages now single page

--- view.php
+++ view.php
@@ -2,11 +2,9 @@
 if(isset($_GET["t"])){ $t = preg_replace('#[^a-z0-9]#i', '', $_GET['t']); } else { header("location: index.php"); exit(); }
 include("conf.php");
 include("header.php");
-if ($t != "cats") {  if ($t != "dogs") { echo "Error"; exit(); } }
-
+if ($t != "cats") {  if ($t != "dogs") { header("location: index.php"); exit(); } }
 if ($t == "cats") { echo '<h1>Cats for adoption</h1><p>You will need a letter from your landlord if you are renting giving permission to keep an animal. When adopting a cat or kitten please remember to bring a cat carry basket as this is the only safe way of transporting a cat or kitten.</p>'; }
 if ($t == "dogs") { echo '<h1>Dogs for adoption</h1><p>You will need a letter from your landlord if you are renting giving permission to keep an animal. For more information about the dogs please ring the Shelter because we have dogs coming in and being re-homed so quickly that they never make it to the web site.</p>'; }
-
 $counter = 0;
 $sql = "SELECT * FROM ".$t." WHERE id!='0' ORDER BY RAND()";
 $query = mysqli_query($db, $sql);
@@ -22,11 +20,8 @@ echo '<table><tr>';
 	  echo '</center></td><td>&nbsp;</td>';
   	  $counter = $counter +1;
  	  if ($counter =="3") { echo '</tr><tr><td><hr></td><td><hr></td><td><hr></td><td><hr></td><td><hr></td><td><hr></td></tr><tr>'; $counter = 0; }
-}
+	}
 echo "</tr></table><hr><br>";
-echo "<b>If you are interested in adopting one of our ";
-if ($t == "cats") { echo 'cats'; }
-if ($t == "dogs") { echo 'dogs'; }
-echo " above please contact us to arrange an appointment to come and meet in person at our shelter.</b>";
+echo "<b>If you are interested in adopting one of our ".$t." above please contact us to arrange an appointment to come and meet in person at our shelter.</b>";
 include("footer.php");
 ?>
