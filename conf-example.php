3d214ff6514ed0bb20b58594cca1314dc65aabcb
Sean <mail@seanslater.com>
2015-11-02 19:13:04 +1030

    Set $sheltername and $sheltertag variables

--- conf-example.php
+++ conf-example.php
@@ -1,3 +1,6 @@
 <?php
-  $db = new mysqli('localhost:3306', '<database user>', '<database password>', '<database name>');
+ $db = new mysqli('localhost:3306', '<database user>', '<database password>', '<database name>');
+  
+ $sheltername = 'Animal Shelter';
+ $sheltertag = 'Giving abandoned cats and dogs a second chance to find loving homes'; 
  ?>
