d580b948ff4492161808571cb567a18ce489f738
Sean <mail@seanslater.com>
2015-11-02 17:12:57 +1030

    Added pages table

--- install/createdb.php
+++ install/createdb.php
@@ -24,12 +24,26 @@ $table_dogs = "CREATE TABLE IF NOT EXISTS dogs (
 			  gender ENUM('m','f') NOT NULL,
 			  added DATETIME NOT NULL,
               PRIMARY KEY (id),
-			  UNIQUE KEY username (name,id)
+			  UNIQUE KEY name (name,id)
              )";
 $query = mysqli_query($db, $table_dogs);
 if ($query === TRUE) {
-	echo '<h3>Dogs table created OK :) </h3><a href="insertdummydata.php">Click here to fill database with dummy data</a>'; 
+	echo '<h3>Dogs table created OK :) </h3>'; 
 } else {
-	echo '<h3>Dogs table NOT created :( </h3><a href="./">Click to go back and try again</a>'; 
+	echo '<h3>Dogs table NOT created :( </h3>'; 
+}
+$table_pages = "CREATE TABLE IF NOT EXISTS pages (
+              id INT(11) NOT NULL AUTO_INCREMENT,
+			  pagename VARCHAR(20) NOT NULL,
+   			  title VARCHAR(250) NOT NULL,
+  			  text VARCHAR(2500) NOT NULL,
+              PRIMARY KEY (id),
+			  UNIQUE KEY pagename (pagename,id)
+             )";
+$query = mysqli_query($db, $table_pages);
+if ($query === TRUE) {
+	echo '<h3>Pages table created OK :) </h3><a href="insertdummydata.php">Click here to fill database with dummy data</a>'; 
+} else {
+	echo '<h3>Pages table NOT created :( </h3><a href="./">Click to go back and try again</a>'; 
 }
 ?>
