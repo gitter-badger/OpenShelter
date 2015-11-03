3f206257f04c4141ef2015b30ce237e512f4120c
Sean <mail@seanslater.com>
2015-11-03 17:49:00 +1030

    Added comments for configuration options

--- conf-example.php
+++ conf-example.php
@@ -1,7 +1,17 @@
-<?php $db = new mysqli('localhost:3306', '<database user>', '<database password>', '<database name>');
-  
- $sheltername = 'Animal Shelter';
- $sheltertag = 'Giving abandoned cats and dogs a second chance to find loving homes'; 
+ <?php
+///////////////////////////
+// Configuration Options //
+///////////////////////////
+
+// Database connection
+	$db = new mysqli('localhost:3306', '<database user>', '<database password>', '<database name>');
+
+// Shelter Name
+	$sheltername = 'Animal Shelter';
  
- $showopenshelter ='1';
+// Shelter Tagline
+	$sheltertag = 'Giving abandoned cats and dogs a second chance to find loving homes'; 
+ 
+// Display link to OpenShelter in footer (1=Yes 0=No)
+	$showopenshelter ='1';
  ?>
