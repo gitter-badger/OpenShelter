44d9422f770bc746ef1c39b59231c5007bff14d7
Sean <mail@seanslater.com>
2015-11-04 04:13:24 +1030

    Re-factored footer

--- include/footer.php
+++ include/footer.php
@@ -1,9 +1,7 @@
 <?php
 	echo '</table><br><b>';
 	include("nav.php");
-	echo '</b><br><br>';
-?>
-&copy; <?php echo date("Y") ?> <a href="./"><?php	echo $sheltername.'</a>';
-	if ($showopenshelter == '1') { echo ' | Powered by <a href="http://seanslater.com/OpenShelter">OpenShelter</a>'; }
-	echo '</html>';
+	echo '</b><br><br>&copy '. date("Y") ?> <a href="./"><?php	echo $sheltername.'</a>';
+		if ($showopenshelter == '1') { echo ' | Powered by <a href="http://seanslater.com/OpenShelter">OpenShelter</a>'; }
+	echo '<br><br></html>';
 ?>
