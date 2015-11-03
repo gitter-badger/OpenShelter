3254ee7ff5665e94a0418e10845fdc127f29b646
Sean <mail@seanslater.com>
2015-11-03 18:22:52 +1030

    Disabled cats and dogs images from front page

--- index.php
+++ index.php
@@ -13,22 +13,22 @@ while($row = mysqli_fetch_array($query)) {
 	  echo "<h1>".$row['title']."</h1>";
 	  echo $row['text'];
 	}
-$sql = "SELECT * FROM cats WHERE photo!='' ORDER BY RAND() LIMIT 3";
-$query = mysqli_query($db, $sql);
-echo '<h1>Cats for adoption</h1><table><tr>';
-	while($row = mysqli_fetch_array($query)) {
-	  echo '<td><center><a href="view.php?t=cats"><img width="200" src="' .$row['photo']. '"></a><h1>';
-	  echo $row['name'];
-	  echo '</h1></center></td>';
-	}
-echo "</tr></table>";
-$sql = "SELECT * FROM dogs WHERE photo!='' ORDER BY RAND() LIMIT 3";
-$query = mysqli_query($db, $sql);
-echo '<h1>Dogs for adoption</h1><table><tr>';
-	while($row = mysqli_fetch_array($query)) {
-	  echo '<td><center><a href="view.php?t=dogs"><img width="200" src="' .$row['photo']. '"></a><h1>';
-	  echo $row['name'];
-	  echo '</h1></center></td>';
-	}
+// $sql = "SELECT * FROM cats WHERE photo!='' ORDER BY RAND() LIMIT 3";
+// $query = mysqli_query($db, $sql);
+// echo '<h1>Cats for adoption</h1><table><tr>';
+// 	while($row = mysqli_fetch_array($query)) {
+// 	  echo '<td><center><a href="view.php?t=cats"><img width="200" src="' .$row['photo']. '"></a><h1>';
+// 	  echo $row['name'];
+// 	  echo '</h1></center></td>';
+// 	}
+// echo "</tr></table>";
+// $sql = "SELECT * FROM dogs WHERE photo!='' ORDER BY RAND() LIMIT 3";
+// $query = mysqli_query($db, $sql);
+// echo '<h1>Dogs for adoption</h1><table><tr>';
+// 	while($row = mysqli_fetch_array($query)) {
+// 	  echo '<td><center><a href="view.php?t=dogs"><img width="200" src="' .$row['photo']. '"></a><h1>';
+// 	  echo $row['name'];
+// 	  echo '</h1></center></td>';
+// 	}
 echo "</tr></table>";
 include("include/footer.php"); ?>
