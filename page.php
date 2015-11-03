4d69ae42f157272369a11e75433981424ccd07f9
Sean <mail@seanslater.com>
2015-11-03 17:41:59 +1030

    Single row for display page with no sidebar

--- page.php
+++ page.php
@@ -1,5 +1,5 @@
 <?php if(isset($_GET["p"])){ $p = preg_replace('#[^a-z0-9]#i', '', $_GET['p']); } else { header("location: index.php"); exit(); } include("conf.php"); include("include/header.php");
 $sql = "SELECT * FROM pages WHERE pagename='$p' LIMIT 1";
 $query = mysqli_query($db, $sql);
-while($row = mysqli_fetch_array($query)) { echo "<h1>".$row['title']."</h1>"; echo $row['text']; }
+while($row = mysqli_fetch_array($query)) { echo "<tr><td><h1>".$row['title']."</h1>"; echo $row['text'] ; echo '</td></tr>'; } 
 include("include/footer.php"); ?>
