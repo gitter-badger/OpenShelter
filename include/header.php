ef23b5c4154add7c139d7358a8318dc39d05da37
Sean <mail@seanslater.com>
2015-11-03 17:41:06 +1030

    Remove call to include sidebar in header

--- include/header.php
+++ include/header.php
@@ -6,9 +6,4 @@
 <br><h1><?php echo $sheltername ?></h1><h2><?php echo $sheltertag ?></h2>
 <?php include("nav.php"); ?>
 <br><br>
-<table width="800"><tr>
-<td width="300" valign="top">
-<?php $sql = "SELECT * FROM pages WHERE pagename='sidebar' LIMIT 1";
-$query = mysqli_query($db, $sql);
-while($row = mysqli_fetch_array($query)) { echo $row['text']; } ?>
-</td><td>&nbsp;</td><td>
+<table width="800">
