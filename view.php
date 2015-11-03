b2a123285287e8f1d40a9904e035b82620aca9d6
Sean <mail@seanslater.com>
2015-11-03 18:17:50 +1030

    Fixed page width to 800 pixels

--- view.php
+++ view.php
@@ -1,7 +1,7 @@
 <?php if(isset($_GET["t"])){ $t = preg_replace('#[^a-z0-9]#i', '', $_GET['t']); } else { header("location: index.php"); exit(); } include("conf.php"); include("include/header.php");
 if ($t != "cats") {  if ($t != "dogs") { header("location: index.php"); exit(); } }
-if ($t == "cats") { echo '<h1>Cats for adoption</h1><p>You will need a letter from your landlord if you are renting giving permission to keep an animal. When adopting a cat or kitten please remember to bring a cat carry basket as this is the only safe way of transporting a cat or kitten.</p>'; }
-if ($t == "dogs") { echo '<h1>Dogs for adoption</h1><p>You will need a letter from your landlord if you are renting giving permission to keep an animal. For more information about the dogs please ring the shelter because we have dogs coming in and being re-homed so quickly that they never make it to the web site.</p>'; }
+if ($t == "cats") { echo '<table width=800><tr><td><h1>Cats for adoption</h1><p>You will need a letter from your landlord if you are renting giving permission to keep an animal. When adopting a cat or kitten please remember to bring a cat carry basket as this is the only safe way of transporting a cat or kitten.</p>'; }
+if ($t == "dogs") { echo '<table width=800><tr><td><h1>Dogs for adoption</h1><p>You will need a letter from your landlord if you are renting giving permission to keep an animal. For more information about the dogs please ring the shelter because we have dogs coming in and being re-homed so quickly that they never make it to the web site.</p>'; }
 $counter = 0;
 $sql = "SELECT * FROM ".$t." WHERE id!='0' ORDER BY RAND()";
 $query = mysqli_query($db, $sql);
@@ -19,5 +19,5 @@ echo '<hr><table><tr>';
   	  $counter = $counter +1;
  	}
 echo "</tr></table><hr><br>";
-echo "<b>If you are interested in adopting one of our ".$t." above please contact us to arrange an appointment to come and meet in person at our shelter.</b>";
+echo "<center><b>If you are interested in adopting one of our ".$t." above please contact us for more information.</b></center></td></tr></table>";
 include("include/footer.php"); ?>
