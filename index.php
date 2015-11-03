9234fb556c32fce98c05e441a30f96bd036f97b4
Sean <mail@seanslater.com>
2015-11-03 17:41:25 +1030

    Call to include sidebar on front page

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php include("conf.php"); include("include/header.php");
+?>
+<tr>
+<td width="300" valign="top">
+<?php $sql = "SELECT * FROM pages WHERE pagename='sidebar' LIMIT 1";
+$query = mysqli_query($db, $sql);
+while($row = mysqli_fetch_array($query)) { echo $row['text']; } ?>
+</td><td>&nbsp;</td><td>
+<?php
 $sql = "SELECT * FROM pages WHERE pagename='news' LIMIT 1";
 $query = mysqli_query($db, $sql);
 while($row = mysqli_fetch_array($query)) {
