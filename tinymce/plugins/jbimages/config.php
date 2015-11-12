5b380f30a28508aac2efb403e5f005c779803ac1
Sean <mail@seanslater.com>
2015-11-13 01:04:28 +1030

    JustBoil.me Images Plugin for TinyMCE

---
+++ tinymce/plugins/jbimages/config.php
@@ -0,0 +1,11 @@
+<?php
+	$config['img_path'] = '../images'; 
+	$config['upload_path'] = $_SERVER['DOCUMENT_ROOT'] . $config['up_path']; 
+	$config['allowed_types'] = 'gif|jpg|png';
+	$config['max_size'] = 0;
+	$config['max_width'] = 0;
+	$config['max_height'] = 0;
+	$config['allow_resize'] = TRUE;
+	$config['encrypt_name'] = FALSE;
+	$config['overwrite'] = FALSE;
+?>
